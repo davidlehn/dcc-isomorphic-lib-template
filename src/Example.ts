7448c3494115e2b6f8197ce6ed4322f74f8595ae
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-12-13 17:56:12 -0500

    Add example DB repo import.

--- src/Example.ts
+++ src/Example.ts
@@ -1,8 +1,12 @@
 /*!
  * Copyright (c) 2022 Digital Credentials Consortium. All rights reserved.
  */
+import {Ed25519VerificationKey2020} from '@digitalbazaar/ed25519-verification-key-2020';
+
 export class Example {
-  public hello(): string {
-    return 'world'
+  public async hello(): Promise<any> {
+    const edKeyPair = await Ed25519VerificationKey2020.generate();
+    // return 'world'
+    return edKeyPair
   }
 }
