d3752589e686234dd1cedd53afe6fe8ed064b516
David I. Lehn <dlehn@digitalbazaar.com>
2022-12-13 22:26:07 -0500

    Update to module.
    - Add "type": "module".
    - Add "exports" field.
    - Remove CJS support.
    - Add mocha "ts-node/esm" loader.
    - Remove repeated mocha command arguments.
    - Remove TS_NODE_COMPILER_OPTIONS.
    - Fix module path issues.
    - Move karma conf to a .cjs.
    - Misc tsconfig changes and ordering.

--- test/Example.spec.ts
+++ test/Example.spec.ts
@@ -1,9 +1,10 @@
 import { expect } from 'chai'
-import { Example } from '../src'
+import { Example } from '../src/index.js'
 
 describe('Example', () => {
   it('calls function', async () => {
     const ex = new Example()
-    expect(ex.hello()).to.equal('world')
+    const d = await ex.hello();
+    expect(d).to.have.property('type', 'Ed25519VerificationKey2020');
   })
 })
